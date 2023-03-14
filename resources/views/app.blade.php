fde976ea1a21dd68606c3e4aef3a843310148b4b
Noel Emaas <90034393+NoelEmaas@users.noreply.github.com>
2023-03-14 23:08:18 +0800

    fixed responsiveness

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,36 +20,36 @@
                 <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white font-logo-font" style="font-family: 'Fira Sans', sans-serif;">eSkolar</span>
             </a>
             <div class="flex md:order-2">
-                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0">Get started</button>
                 <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
             </button>
             </div>
             <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-                <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-16 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 lg:space-x-16 xl:space-x-16 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
                     <li>
                         <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
                     </li>
                     <li>
-                        <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Scholarships</a>
+                        <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Scholarships</a>
                     </li>
                     <li>
-                        <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Forums</a>
+                        <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Forums</a>
                     </li>
                     <li>
-                        <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                        <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">About</a>
                     </li>
                     <li>
-                        <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                        <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Contact</a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
 
-    <div class="h-screen w-screen">
-        <div class="flex flex-col lg:flex-row w-screen h-screen">
+    <div class="w-screen 2xl:pt-20 xl:pt-20 lg:pt-20">
+        <div class="flex flex-col lg:flex-row w-screen 2xl:pl-10">
           <div class="lg:w-1/2 xl:p-20 p-12 pt-20 md: flex items-center justify-center flex-col">
             <h1 class="2xl:text-7xl text-6xl font-bold text-gray-800">
               <span>Empowering</span> students to bring them closer to achieving their dreams.
@@ -76,12 +76,12 @@
         </div>
     </div>
       
-    <div class="h-screen w-screen pt-16">
-        <h1 class="xl:text-6xl text-4xl text-center font-bold">What's Our Purpose</h1>
-        <p class="text-center w-[65%] pt-4 mx-auto">This tool aims to simplify the scholarship search process for students by providing reliable information and instructions for legitimate scholarships. Additionally, it aims to assist educational institutions in providing accurate information to students seeking financial assistance.</p>
-        <div class="flex flex-wrap justify-center items-center w-full lg:space-x-14 mt-20 px-10 md:px-0 2xl:space-y-0 xl:space-y-0 lg:space-y-0 space-y-4 min-w-sm">
-            <div class="2xl:w-[400px] 2xl:h-[500px] lg:w-[300px] lg:h-[400px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <div class="h-[50%] 2xl:h-[60%]">
+    <div class="w-screen pt-16">
+        <h1 class="xl:text-6xl text-5xl text-center font-bold">What's Our Purpose</h1>
+        <p class="text-center xl:w-[65%] md:w-[85%] sm:w-[85%] w-[85%] pt-4 mx-auto">This tool aims to simplify the scholarship search process for students by providing reliable information and instructions for legitimate scholarships. Additionally, it aims to assist educational institutions in providing accurate information to students seeking financial assistance.</p>
+        <div class="flex flex-wrap justify-center items-center w-full lg:space-x-10 mt-20 lg:px-1 px-10 md:px-0 md:space-y-0 md:space-x-[1%] 2xl:space-y-0 xl:space-y-0 lg:space-y-0 space-y-4 min-w-sm">
+            <div class="2xl:w-[400px] 2xl:h-[500px] lg:w-[300px] lg:h-[400px] md:w-[250px] md:h-[380px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div class="2xl:h-[60%] xl:h-[50%] lg:h-[50%] md:h-[50%] 2x:block xl:block lg:block md:block hidden">
                   <img class="rounded-t-lg w-full h-full object-cover" src="{{ asset('assets/search.png') }}" alt="" />
                 </div> 
                 <div class="p-5 h-[50%] 2xl:h-[40%]">
@@ -95,8 +95,8 @@
                   </a>
                 </div>
             </div>
-            <div class="2xl:w-[400px] 2xl:h-[500px] lg:w-[300px] lg:h-[400px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <div class="h-[50%] 2xl:h-[60%] p-10">
+            <div class="2xl:w-[400px] 2xl:h-[500px] lg:w-[300px] lg:h-[400px] md:w-[250px] md:h-[380px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div class="2xl:h-[60%] xl:h-[50%] lg:h-[50%] md:h-[50%] 2x:block xl:block lg:block md:block hidden p-10">
                   <img class="rounded-t-lg w-full h-full object-cover" src="{{ asset('assets/post.png') }}" alt="" />
                 </div> 
                 <div class="p-5 h-[50%] 2xl:h-[40%]">
@@ -111,8 +111,8 @@
                 </div>
             </div>
 
-            <div class="2xl:w-[400px] 2xl:h-[500px] lg:w-[300px] lg:h-[400px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <div class="h-[50%] 2xl:h-[60%] p-10">
+            <div class="2xl:w-[400px] 2xl:h-[500px] lg:w-[300px] lg:h-[400px] md:w-[250px] md:h-[380px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div class="2xl:h-[60%] xl:h-[50%] lg:h-[50%] md:h-[50%] 2x:block xl:block lg:block md:block hidden p-10">
                   <img class="rounded-t-lg w-full h-full object-cover" src="{{ asset('assets/forum.jpg') }}" alt="" />
                 </div> 
                 <div class="p-5 h-[50%] 2xl:h-[40%]">
