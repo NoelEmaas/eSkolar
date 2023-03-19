606dfd0fe7da7d54d32bf62cae7d3b79f5feafe4
Noel Emaas <90034393+NoelEmaas@users.noreply.github.com>
2023-03-19 23:36:38 +0800

    added card component for post

---
+++ resources/views/components/card-view-full-post.blade.php
@@ -0,0 +1,92 @@
+<div class="col-12 col-xxl-10 col-xl-19 col-lg-10 col-md-10 mb-4 mx-auto">
+    <div class="card">
+      <div class="card-header py-3 bg-white">
+          <div class="d-flex justify-content-between">
+              <div class="container">
+                  <div class="row">
+                      <div class="col-auto">
+                          <img src="https://bootdey.com/img/Content/avatar/avatar3.png" class="d-block ui-w-40 rounded-circle" alt="">
+                      </div>
+                      <div class="col-auto">
+                          <div class="media-body ml-3">
+                              Kenneth Frazier
+                              <div class="text-muted small">3 days ago</div>
+                            </div>
+                      </div>
+                  </div>
+              </div>
+              <i class="bi bi-three-dots-vertical fs-3 text-muted" style="cursor: pointer"></i>
+          </div>
+      </div>
+      <div class="card-body p-4">
+          <div class="title mb-3">
+              <h5 class="card-title">CHED</h5>
+              <p class="card-text text-muted">CHED Scholarship Program 2020-2021</p>
+          </div>
+          <p class="amount mb-3">Amount: Php 80,000 - Php 120,000</p>
+          <div>
+              <p>For qualified and deserving students entering first year of Tertiary Education whose General Weighted Average (GWA) is at least 96% or its equivalent (For Full Merit Program) and 93%-95% or its equivalent for Half Merit Program. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus rem aliquam cum? Eaque et illum reiciendis doloremque eum? Omnis similique voluptatibus minima, neque error aut nostrum illum fuga veritatis natus quo minus. Repellendus ipsum laudantium accusamus nisi, hic repudiandae, commodi debitis voluptates est iste earum soluta repellat non totam dolorem minus, tempore amet recusandae libero aperiam? Fugit eveniet doloremque eaque exercitationem dolorem totam impedit consequuntur laudantium repellendus inventore earum velit ipsum aperiam accusamus, quis rem vero temporibus suscipit adipisci quam tempore alias quasi aliquam? Error, vitae. Animi, voluptate quod, error maxime magnam dolorem aspernatur est nam minima fugiat obcaecati eaque. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur illum eum impedit ex voluptatem voluptatum, eligendi quod voluptate saepe eius soluta id sit, laborum dolores ullam incidunt tenetur aliquam autem minima recusandae accusantium corrupti vero perferendis. Dolores fugiat architecto, necessitatibus accusantium vel quia perspiciatis commodi fugit pariatur ex quod illo iure nam molestiae, itaque consequuntur, dolor tenetur facilis eius recusandae sit! Et minus tempora ratione architecto! Aut recusandae optio rem vel eos, repellat provident sequi facere itaque neque quos accusantium nemo odit perspiciatis consequatur illo est quas commodi possimus. In, ratione reiciendis quos, assumenda magni necessitatibus dolorem eum aliquid optio fugiat inventore sapiente? Voluptatem beatae provident odit eligendi praesentium illo autem molestiae ad! Quidem dolorem illo perspiciatis rem. Dolorem dignissimos maxime dolor molestiae quas perspiciatis impedit ullam vel harum vero. Dolorem optio quaerat repellat nobis enim magnam aliquam numquam quis voluptates expedita molestiae, laborum dicta, hic facere labore cumque sed doloremque harum commodi nam fugit excepturi in! Asperiores neque maiores repellendus blanditiis pariatur vitae nemo in quo reprehenderit tempore, ipsa commodi perferendis optio culpa inventore nam, quod cum laborum! Ducimus cupiditate quia repudiandae voluptates, voluptatibus rem, sed laborum expedita harum consequuntur sunt fugiat reiciendis, id temporibus quae ullam illum blanditiis? </p>
+          </div>
+      </div>
+      <div class="card-footer bg-white px-4 pt-3 justify-content-center">
+          <div class="d-flex justify-content-between mb-4">
+              <div>
+                  <i class="bi bi-star" style="cursor: pointer"></i>
+                  <span class="px-2">13</span>
+              </div>
+              <span style="cursor: pointer">13 comments</span>
+          </div>
+
+        <div class="d-flex flex-start p-4 rounded mb-3" style="background-color: #f5f5f5;">
+            <img class="rounded-circle shadow-1-strong me-3 d-block"
+                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(23).webp" alt="avatar" width="60"
+                height="40" />
+            <div>
+                <h6 class="fw-bold mb-1">Maggie Marsh</h6>
+                <div class="d-flex align-items-center mb-3">
+                    <p class="mb-0 small text-muted">
+                        March 07, 2021
+                    </p>
+                    <a href="#!" class="link-muted"><i class="fas fa-pencil-alt ms-2"></i></a>
+                    <a href="#!" class="link-muted"><i class="fas fa-redo-alt ms-2"></i></a>
+                    <a href="#!" class="link-muted"><i class="fas fa-heart ms-2"></i></a>
+                </div>
+                <p class="mb-0">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting
+                    industry. Lorem Ipsum has been the industry's standard dummy text ever
+                    since the 1500s, when an unknown printer took a galley of type and
+                    scrambled it.
+                </p>
+            </div>
+        </div>
+
+          
+        <div class="d-flex flex-start p-4 rounded mb-3" style="background-color: #f5f5f5;">
+            <img class="rounded-circle shadow-1-strong me-3 d-block"
+                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(23).webp" alt="avatar" width="60"
+                height="40" />
+            <div>
+                <h6 class="fw-bold mb-1">Maggie Marsh</h6>
+                <div class="d-flex align-items-center mb-3">
+                    <p class="mb-0 small text-muted">
+                        March 07, 2021
+                    </p>
+                    <a href="#!" class="link-muted"><i class="fas fa-pencil-alt ms-2"></i></a>
+                    <a href="#!" class="link-muted"><i class="fas fa-redo-alt ms-2"></i></a>
+                    <a href="#!" class="link-muted"><i class="fas fa-heart ms-2"></i></a>
+                </div>
+                <p class="mb-0">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting
+                    industry. Lorem Ipsum has been the industry's standard dummy text ever
+                    since the 1500s, when an unknown printer took a galley of type and
+                    scrambled it.
+                </p>
+            </div>
+        </div>
+          
+
+
+
+      </div>
+    </div>
+</div>
