c3605ffe682bb91740cd2dd849dc5b8d0a49877a
Noel Emaas <nemaas@gbox.adnu.edu.ph>
2023-04-10 19:08:23 +0800

    added create post modal

--- resources/views/scholarships.blade.php
+++ resources/views/scholarships.blade.php
@@ -20,35 +20,46 @@
         aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
-                <div class="modal-header border-bottom-1 py-4 px-4">
-                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Sign in to our platform</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <div class="modal-header py-2 px-5 d-flex justify-content-start align-items-center" style="background-color: #677BD7;">
+                    <p class="fs-5 fw-bold text-light pt-3">Help other students</p>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
+                <div class="modal-body px-xxl-5 px-xl-5 px-lg-5 px-md-5 px-2 pb-4">
                     <form>
                         <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Email address</label>
+                            <label for="exampleInputEmail1" class="form-label">Benefactor</label>
                             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                                placeholder="name@company.com">
-                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                                placeholder="CHED">
                         </div>
                         <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="••••••••">
+                            <label for="exampleInputPassword1" class="form-label">Scholarship Program</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Ched Scholarship Program 2021">
                         </div>
-                        <div class="mb-3 pt-2 form-check">
-                            <div class="d-flex justify-content-between">
-                                <div>
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                    <label class="form-check-label" for="exampleCheck1">Remember me</label>
+                        <div class="mb-3">
+                            <label for="inputAmount" class="form-label">Amount Of Assistance</label>
+                            <div class="row" id="inputAmount">
+                                <div class="input-group col">
+                                    <span class="input-group-text">₱</span>
+                                    <input type="number" class="form-control" aria-label="Amount (to the nearest dollar)" placeholder="30000">
+                                    <span class="input-group-text">.00</span>
                                 </div>
-                                <a href="#" style="text-decoration: none">Lost password?</a>
+                                <div class="col-auto d-flex align-items-center">
+                                    -
+                                </div>
+                                <div class="input-group col">
+                                    <span class="input-group-text">₱</span>
+                                    <input type="number" class="form-control" aria-label="Amount (to the nearest dollar)" placeholder="60000">
+                                    <span class="input-group-text">.00</span>
+                                </div>
                             </div>
                         </div>
-                        <button type="submit" class="btn btn-primary w-100 mt-3">Login to your account</button>
-                        <div class="mt-3">
-                            Not registered? <a href="#" style="text-decoration: none" data-bs-toggle="modal"
-                                data-bs-target="#sign-up-form">Create an account</a>
+                        <div class="mb-5">
+                            <label for="exampleInputPassword1" class="form-label">Scholarship Information</label>
+                            <textarea class="form-control" placeholder="..." style="height: 200px"></textarea>
+                        </div>
+                        <div class="d-flex justify-content-end mt-4">
+                            <button class="btn me-5">Discard</button>
+                            <button class="btn px-4 py-2" style="background-color: #677BD7; color: white;">Post Scholarship</button>
                         </div>
                     </form>
                 </div>
@@ -61,29 +72,39 @@
 
     <div class="containter-fluid border border-bottom-1 pt-5 pb-4" style="height: auto; background-color: #FBFDFF">
         <div class="container">
-            <div class="row mt-5 pt-4 mb-4">
-                <div class="col search-col">
-                    <div class="search">
-                        <input type="text" class="form-control w-100" placeholder="Find scholarship"> 
-                        <a class="mt-2 dropdown" id="filter-dropdown" style="margin-right: 70px;" role="button">
-                            <i class="bi bi-funnel text-muted " style="font-size: 1.5rem;" data-bs-toggle="dropdown" aria-expanded="false"></i>
-                            <ul class="dropdown-menu">
-                                <li class="dropdown-item">noel</li>
-                                <li class="dropdown-item">noel</li>
-                                <li class="dropdown-item">noel</li>
-                            </ul>
-                        </a>
-                        <button class="btn btn-primary"><i class="bi bi-search"></i></button>
-                    </div>
+            <div class="row g-3 mt-5 pt-4 mb-4">
+
+                <div class="col-sm input-group input-group-lg">
+                    <input type="text" class="form-control" aria-describedby="inputGroup-sizing-lg" placeholder="Search Scholarship" style="font-size: 14px;">
+                    <span class="input-group-text px-4 py-3" id="inputGroup-sizing-lg" role="button" style="font-size: 14px; background-color: #677BD7; color: white;">
+                        Search
+                    </span>
                 </div>
-                <div class="col-auto vr" style="padding: 0; margin-right: 10px;"></div>
-                <button class="btn-add-post-sm btn btn-primary col-auto" style="margin-right: 10px;" data-bs-toggle="modal" data-bs-target="#createPostModal"><i class="bi bi-pencil-fill"></i></button>
-                <button class="btn-add-post btn btn-primary col-auto" style="margin-right: 10px;" data-bs-toggle="modal" data-bs-target="#createPostModal">Create Post</button>
+
+
+                <div class="col-xxl-auto col-xl-auto col-lg-auto col-md-auto col-sm-auto col-6 btn-group">
+                    <button type="button" class="btn dropdown-toggle btn-filter px-5" data-bs-toggle="dropdown" aria-expanded="false" style="width: 100%; height: 100%;">
+                      Filter
+                    </button>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="#">Another action</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="#">Something</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="#">Separated link</a></li>
+                    </ul>
+                 </div>
+
+                <div class="col-xxl-auto col-xl-auto col-lg-auto col-md-auto col-sm-auto col-6">
+                    <button class="btn-add-post btn btn-primary" style="width: 100%; height: 100%;" data-bs-toggle="modal" data-bs-target="#createPostModal">Create Post <i class="bi bi-plus-lg ps-1"></i></button> 
+                </div>
+    
             </div>
         </div>
     </div>
 
-    
 
     <div class="container mt-4">
         <div class="row">
@@ -100,5 +121,4 @@
     <x-footer />
 
 </body>
-
 </html>
