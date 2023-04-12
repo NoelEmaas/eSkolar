5e5ce17563223a65f89cc748e2c37d810ea99949
Noel Emaas <nemaas@gbox.adnu.edu.ph>
2023-04-12 18:23:44 +0800

    added user button in navbar when logged in

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -28,8 +28,79 @@
                         aria-current="page" href="/contact">Contact</a>
                 </li>
             </ul>
-            <button class="btn mx-2 gs-btn" type="submit" style="width: 120px; font-size: 14px;" data-bs-toggle="modal"
-                data-bs-target="#sign-in-form">Sign In</button>
+            <hr class="d-xxl-none d-xl-none d-lg-none d-block mx-1">
+            @if(Auth::check())
+                @auth
+                <div class= "d-xxl-flex d-xl-flex d-lg-flex d-none">
+                    <img src="https://bootdey.com/img/Content/avatar/avatar3.png"
+                    class="rounded-circle" alt="" width="35" height="35" role="button" data-bs-html="true"
+                    data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom"
+                    data-bs-title=
+                    '
+                    <div class="media-body m-auto pt-1 py-2">
+                        <div class="d-flex" >
+                            {{ Auth::user()->first_name }}
+                            {{ Auth::user()->last_name }}
+                        </div>
+                        <div class="text-muted small">{{ Auth::user()->email }}</div>
+                    </div>
+                    '
+                    data-bs-content=
+                    ' 
+                    <ul class="list-group list-group-flush rounded">
+                        <li class="list-group-item list-group-item-action border border-0"><i class="bi bi-person me-1 fs-5"></i> Your Profile</li>
+                        <li class="list-group-item list-group-item-action border border-0"><i class="bi bi-collection me-2"></i> Own Posts</li>
+                        <li class="list-group-item list-group-item-action border border-0"><i class="bi bi-chat-square-dots me-2"></i> Own Forums</li>
+                        <li class="list-group-item list-group-item-action"><i class="bi bi-star me-2"></i> Starred Posts</li>
+                        <li class="list-group-item list-group-item-action py-3">Sign Out <i class="bi bi-arrow-right ms-5"></i></li>
+                    </ul>
+                    '>
+                    <i class="bi bi-chevron-down my-auto ms-3"></i>
+                </div>
+                <div class="d-xxl-none d-xl-none d-lg-none d-block bg-light px-3 py-3 mx-1 rounded">
+                    <div class="d-flex justify-content-between" role="button" data-bs-toggle="collapse" data-bs-target="#user-options" aria-controls="user-options">
+                        <div class="d-flex">
+                            <img src="https://bootdey.com/img/Content/avatar/avatar3.png"
+                            class="rounded-circle my-auto" alt="" width="35" height="35">
+                            <div class="ms-3">
+                                <div class="d-flex" >
+                                    {{ Auth::user()->first_name }}
+                                    {{ Auth::user()->last_name }}
+                                </div>
+                                <div class="text-muted small">{{ Auth::user()->email }}</div>
+                            </div>
+
+                        </div>
+                        <i class="bi bi-chevron-down my-auto me-2"></i>
+                    </div>
+                    <div class="collapse mt-3" id="user-options">
+                        <div class="card card-body bg-transparent p-0 m-0">
+                            <ul class="list-group list-group-flush rounded">
+                                <li class="list-group-item list-group-item-action border border-0"><i class="bi bi-person me-1 fs-5"></i> Your Profile</li>
+                                <li class="list-group-item list-group-item-action border border-0"><i class="bi bi-collection me-2"></i> Own Posts</li>
+                                <li class="list-group-item list-group-item-action border border-0"><i class="bi bi-chat-square-dots me-2"></i> Own Forums</li>
+                                <li class="list-group-item list-group-item-action"><i class="bi bi-star me-2"></i> Starred Posts</li>
+                                <li class="list-group-item list-group-item-action py-3"><i class="bi bi-box-arrow-in-left me-2"></i> Sign Out</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                @else
+                    Hello
+                    <button class="btn mx-2 gs-btn" type="submit" style="width: 120px; font-size: 14px;" data-bs-toggle="modal"
+                        data-bs-target="#sign-in-form">Sign In</button>
+                @endauth
+            @endif
         </div>
     </div>
 </nav>
+
+{{-- <script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+
+<script>
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    })
+</script> --}}
