851f785c94ac940acc6548e0648ee4668f6627f3
OjouNii <belarminojiankarlo@gmail.com>
2023-05-12 03:18:31 +0800

    Bug: Removed search functionality, problems with UI

--- resources/views/forums.blade.php
+++ resources/views/forums.blade.php
@@ -3,22 +3,17 @@
 @section('content')
 <div class="containter-fluid pt-4 pb-2" style="height: auto;">
     <div class="container">
-        <form action="{{ route('getForums') }}" method="GET">
-        @csrf
-            <div class="row g-3 mt-5 pt-4">
-                <div class="col-sm input-group input-group-lg border border-0">
-                    <input type="text" class="form-control border border-0" aria-describedby="inputGroup-sizing-lg" name="q" placeholder="Search Discussion" style="font-size: 14px; box-shadow: none; box-shadow: 0px 0px 5px rgb(203, 209, 214);">
-                    <button style="background: none; color: white; border: none; margin: 0; padding: 0;" type="submit">
-                        <span class="input-group-text px-4 py-3 border border-0 bg-blue" id="inputGroup-sizing-lg" role="button" style="font-size: 14px; color: white; box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;">
-                            Search
-                        </span>
-                    </button>
-                </div>
-                <div class="col-xxl-auto col-xl-auto col-lg-auto col-md-auto col-sm-auto col-12">
-                    <button class="btn-add-post btn btn-primary border-0" style="width: 100%; height: 100%; box-shadow: 0px 0px 5px rgb(203, 209, 214);" data-bs-toggle="modal" data-bs-target="#createForumModal">Start Forum <i class="bi bi-chat-square-dots ps-2"></i></button> 
-                </div>
+        <div class="row g-3 mt-5 pt-4">
+            <div class="col-sm input-group input-group-lg border border-0">
+                <input type="text" class="form-control border border-0" aria-describedby="inputGroup-sizing-lg" name="q" placeholder="Search Discussion" style="font-size: 14px; box-shadow: none; box-shadow: 0px 0px 5px rgb(203, 209, 214);">
+                <span class="input-group-text px-4 py-3 border border-0 bg-blue" id="inputGroup-sizing-lg" role="button" style="font-size: 14px; color: white; box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;">
+                    Search
+                </span>
             </div>
-        </form>
+            <div class="col-xxl-auto col-xl-auto col-lg-auto col-md-auto col-sm-auto col-12">
+                <button class="btn-add-post btn btn-primary border-0" style="width: 100%; height: 100%; box-shadow: 0px 0px 5px rgb(203, 209, 214);" data-bs-toggle="modal" data-bs-target="#createForumModal">Start Forum <i class="bi bi-chat-square-dots ps-2"></i></button> 
+            </div>
+        </div>
     </div>
 </div>
 
--- resources/views/scholarships.blade.php
+++ resources/views/scholarships.blade.php
@@ -4,24 +4,20 @@
 <!-- Search Bar & Filter -->
 <div class="containter-fluid pt-4 pb-2" style="height: auto;">
     <div class="container">
-        <form action="{{ route('getScholarships') }}" method="GET">
-        @csrf
-            <div class="col row g-3 mt-5 pt-4">
-                <div class="col-sm input-group input-group-lg border border-0">
-                    <input type="text" class="form-control border border-0" aria-describedby="inputGroup-sizing-lg" name="q" placeholder="Search Scholarship" style="font-size: 14px; box-shadow: none; box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;">
-                    <button style="background: none; color: white; border: none; margin: 0; padding: 0;" type="submit">
-                        <span class="input-group-text px-4 py-3 border border-0 bg-blue" id="inputGroup-sizing-lg" role="button" style="font-size: 14px; color: white; box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;">
-                            Search
-                        </span>
-                    </button>
-                </div>
-                <div class="col-xxl-auto col-xl-auto col-lg-auto col-md-auto col-sm-auto col-12">
-                    <button class="btn-add-post btn btn-primary border border-0" style="width: 100%; height: 100%; box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;" data-bs-toggle="modal" data-bs-target="#createPostModal">Create Post <i class="bi bi-pencil-square ps-1"></i></button> 
-                </div>
+        <div class="col row g-3 mt-5 pt-4">
+            <div class="col-sm input-group input-group-lg border border-0">
+                <input id="searchScholarshipBar" type="text" class="form-control border border-0" aria-describedby="inputGroup-sizing-lg" name="q" placeholder="Search Scholarship" style="font-size: 14px; box-shadow: none; box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;">
+                <span class="input-group-text px-4 py-3 border border-0 bg-blue" id="searchScholarshipButton" role="button" style="font-size: 14px; color: white; box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;">
+                    Search
+                </span>
             </div>
-        </form>
+            <div class="col-xxl-auto col-xl-auto col-lg-auto col-md-auto col-sm-auto col-12">
+                <button class="btn-add-post btn btn-primary border border-0" style="width: 100%; height: 100%; box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;" data-bs-toggle="modal" data-bs-target="#createPostModal">Create Post <i class="bi bi-pencil-square ps-1"></i></button> 
+            </div>
+        </div>
     </div>
 </div>
+
 <!-- End of Search Bar & Filter -->
 
 <!-- Scholarship cards -->
@@ -34,5 +30,4 @@
     </div>
 </div>
 <!-- End of Scholarship cards -->
-
 @endsection
